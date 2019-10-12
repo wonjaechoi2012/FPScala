fe28938775f803855d5667df8d2372935a91f8b2
wonjaechoi <wonjaechoi2014@gmail>
2019-10-12 23:30:20 +0900

    functional programming in scala - ~chapter4 option start

---
+++ src/main/scala/option/option.scala
@@ -0,0 +1,29 @@
+package option
+
+sealed trait Option[+A]{
+  def map[B](f: A=>B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case None => None
+  }
+  def flatMap[B](f: A=>Option[B]): Option[B] = this match {
+    case Some(a) => f(a)
+    case None => None
+  }
+  def getOrElse[B >: A](default: =>B): B = this match {
+    case Some(a) => a
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(a) => this
+    case None => ob
+  }
+
+  def filter(f: A=>Boolean): Option[A] = this match {
+    case Some(a) if(f(a)) => this
+    case None => None
+  }
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
